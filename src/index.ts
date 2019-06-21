f449df65c9413dbf7dbd22ae95af7256f67d37f9
ben-vr <ben.vanraemdonck@icapps.com>
2019-06-21 10:43:40 +0200

    export s3 client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as middlewareService from './lib/middleware';
 import * as localService from './lib/local';
 import * as amazonService from './lib/amazon';
+import * as S3 from 'aws-sdk/clients/s3';
 
 export const middleware = middlewareService;
 export const local = localService;
 export const amazon = amazonService;
+
+export { S3 };
